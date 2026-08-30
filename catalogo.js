1d89b4fda89e327831adff466e72517576a054ba
felipekoch24 <felipekoch2001@gmail.com>
2026-08-30 18:22:17 -0300

    Create catalogo.js

---
+++ catalogo.js
@@ -0,0 +1,19 @@
+const CATALOGO = {
+    "bsd_t3": {
+        "nome": "Bungou Stray Dogs T3",
+        "capa": "https://raw.githubusercontent.com/felipekoch24/TV_STREAM/main/capas/bsd_t3.jpg",
+        "eps": [
+            {"id": "bsd01", "nome": "Episódio 01", "link": "https://raw.githubusercontent.com/felipekoch24/TV_STREAM/main/bsd_t3_ep01/index.m3u8"}
+        ]
+    },
+    "hell_mode_t2": {
+        "nome": "Hell Mode T2",
+        "capa": "https://raw.githubusercontent.com/felipekoch24/TV_STREAM/main/capas/hellmode2.webp",
+        "eps": [
+            {"id": "hm2_06", "nome": "Episódio 06", "link": "https://raw.githubusercontent.com/felipekoch24/TV_STREAM/main/HellMode_T2E6/playlist.m3u8"},
+            {"id": "hm2_07", "nome": "Episódio 07", "link": "https://raw.githubusercontent.com/felipekoch24/TV_STREAM/main/HellMode_T2E7/playlist.m3u8"},
+            {"id": "hm2_08", "nome": "Episódio 08", "link": "https://raw.githubusercontent.com/felipekoch24/TV_STREAM/main/HellMode_T2E8/playlist.m3u8"},
+            {"id": "hm2_09", "nome": "Episódio 09", "link": "https://raw.githubusercontent.com/felipekoch24/TV_STREAM/main/HellMode_T2E9/playlist.m3u8"}
+        ]
+    }
+};
