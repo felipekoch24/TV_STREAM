eb53065fa7853c0957a007345103c7ba8cabf3d8
felipekoch24 <felipekoch2001@gmail.com>
2026-09-09 22:06:42 -0300

    Update link for Episode 10 in catalogo.js

--- catalogo.js
+++ catalogo.js
@@ -14,7 +14,7 @@ const CATALOGO = {
             {"id": "hm2_07", "nome": "Episódio 07", "link": "https://cdn.jsdelivr.net/gh/felipekoch24/TV_STREAM@main/HellMode_T2E7/playlist.m3u8"},
             {"id": "hm2_08", "nome": "Episódio 08", "link": "https://cdn.jsdelivr.net/gh/felipekoch24/TV_STREAM@main/HellMode_T2E8/playlist.m3u8"},
             {"id": "hm2_09", "nome": "Episódio 09", "link": "https://cdn.jsdelivr.net/gh/felipekoch24/TV_STREAM@main/HellMode_T2E9/playlist.m3u8"},
-            {"id": "hm2_10", "nome": "Episódio 10", "link": "https://cdn.jsdelivr.net/gh/felipekoch24/TV_STREAM@main/HellMode_T2E10/playlist.m3u8"}
+            {"id": "hm2_10", "nome": "Episódio 10", "link": "https://cdn.jsdelivr.net/gh/felipekoch24/TV_STREAM@main/HellMode_T2E10/index.m3u8"}
         ]
     }
 };
