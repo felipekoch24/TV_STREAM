7521718bb1d901fe3d12d4eb31756a62e5517a8b
felipekoch24 <felipekoch2001@gmail.com>
2026-09-09 21:25:05 -0300

    Update image links to use jsDelivr CDN

--- catalogo.js
+++ catalogo.js
@@ -1,24 +1,20 @@
 const CATALOGO = {
     "bsd_t3": {
         "nome": "Bungou Stray Dogs T3",
-        "capa": "https://raw.githubusercontent.com/felipekoch24/TV_STREAM/main/capas/bsd_t3.jpg",
+        "capa": "https://cdn.jsdelivr.net/gh/felipekoch24/TV_STREAM@main/capas/bsd_t3.jpg",
         "eps": [
-            {"id": "bsd01", "nome": "Episódio 01", "link": "https://raw.githubusercontent.com/felipekoch24/TV_STREAM/main/bsd_t3_ep01/index.m3u8"}
+            {"id": "bsd01", "nome": "Episódio 01", "link": "https://cdn.jsdelivr.net/gh/felipekoch24/TV_STREAM@main/bsd_t3_ep01/index.m3u8"}
         ]
     },
     "hell_mode_t2": {
         "nome": "Hell Mode T2",
-        "capa": "https://raw.githubusercontent.com/felipekoch24/TV_STREAM/main/capas/hellmode2.webp",
+        "capa": "https://cdn.jsdelivr.net/gh/felipekoch24/TV_STREAM@main/capas/hellmode2.webp",
         "eps": [
-            {"id": "hm2_06", "nome": "Episódio 06", "link": "https://raw.githubusercontent.com/felipekoch24/TV_STREAM/main/HellMode_T2E6/playlist.m3u8"},
-            {"id": "hm2_07", "nome": "Episódio 07", "link": "https://raw.githubusercontent.com/felipekoch24/TV_STREAM/main/HellMode_T2E7/playlist.m3u8"},
-            {"id": "hm2_08", "nome": "Episódio 08", "link": "https://raw.githubusercontent.com/felipekoch24/TV_STREAM/main/HellMode_T2E8/playlist.m3u8"},
-            {"id": "hm2_09", "nome": "Episódio 09", "link": "https://raw.githubusercontent.com/felipekoch24/TV_STREAM/main/HellMode_T2E9/playlist.m3u8"},
-            {"id": "hm2_10", "nome": "Episódio 10", "link": "https://raw.githubusercontent.com/felipekoch24/TV_STREAM/main/HellMode_T2E10/playlist.m3u8"}
+            {"id": "hm2_06", "nome": "Episódio 06", "link": "https://cdn.jsdelivr.net/gh/felipekoch24/TV_STREAM@main/HellMode_T2E6/playlist.m3u8"},
+            {"id": "hm2_07", "nome": "Episódio 07", "link": "https://cdn.jsdelivr.net/gh/felipekoch24/TV_STREAM@main/HellMode_T2E7/playlist.m3u8"},
+            {"id": "hm2_08", "nome": "Episódio 08", "link": "https://cdn.jsdelivr.net/gh/felipekoch24/TV_STREAM@main/HellMode_T2E8/playlist.m3u8"},
+            {"id": "hm2_09", "nome": "Episódio 09", "link": "https://cdn.jsdelivr.net/gh/felipekoch24/TV_STREAM@main/HellMode_T2E9/playlist.m3u8"},
+            {"id": "hm2_10", "nome": "Episódio 10", "link": "https://cdn.jsdelivr.net/gh/felipekoch24/TV_STREAM@main/HellMode_T2E10/playlist.m3u8"}
         ]
     }
-<<<<<<< HEAD
 };
-=======
-};
->>>>>>> 327a5fec2830ef0abadc17a3fe311cc3815fa4b9
